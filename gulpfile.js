31802abf029ba1ee5b0dafb7b81fa24645d48d68
Garth Jager <rantechnologies@gmail.com>
2017-10-12 08:20:26 +0200

    Added image compression

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@ var gulp = require('gulp'),
     minifyHTML = require('gulp-minify-html'),
     uglify = require('gulp-uglify'),
     jsonminify = require('gulp-jsonminify'),
+    imagemin = require('gulp-imagemin'),
+    pngcrush = require('imagemin-pngcrush'),
     concat = require('gulp-concat');
 
 var env,
@@ -87,12 +89,24 @@ gulp.task('json', function() {
   .pipe(connect.reload())
 });
 
+gulp.task('images', function() {
+  gulp.src('builds/development/images/**/*.*')
+  .pipe(gulpif(env === 'production', imagemin({
+    progressive: true,
+    svgoPlugins: [{removeViewBox: false}],
+    use: [pngcrush()]
+  })))
+  .pipe(gulpif(env === 'production', gulp.dest(outputDir + 'images')))
+  .pipe(connect.reload())
+});
+
 gulp.task('watch', function() {
   gulp.watch(coffeeSources, ['coffee']);
   gulp.watch(jsSources, ['js']);
   gulp.watch('components/sass/*.scss', ['compass']);
   gulp.watch('builds/development/*.html', ['html']);
   gulp.watch('builds/development/js/*.json', ['json']);
+  gulp.watch('builds/development/images/**/*.*', ['images']);
 });
 
 gulp.task('connect', function() {
@@ -102,5 +116,5 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'json', 'coffee', 'js', 'compass', 'images', 'connect', 'watch']);
 
